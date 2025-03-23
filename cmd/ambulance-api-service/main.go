b1196542d32736d7c1034c9dbd6f4c3081413ba8
mdudak <mato.dudak@gmail.com>
2025-03-23 17:31:00 +0100

    ambulance waiting list

--- cmd/ambulance-api-service/main.go
+++ cmd/ambulance-api-service/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/xdudakm-wac/ambulance-webapi/api"
 	"github.com/xdudakm-wac/ambulance-webapi/internal/ambulance_wl"
+	"github.com/xdudakm-wac/ambulance-webapi/internal/db_service"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -21,10 +25,30 @@ func main() {
 	}
 	engine := gin.New()
 	engine.Use(gin.Recovery())
+
+	corsMiddleware := cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "PUT", "POST", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Origin", "Authorization", "Content-Type"},
+		ExposeHeaders:    []string{""},
+		AllowCredentials: false,
+		MaxAge:           12 * time.Hour,
+	})
+	engine.Use(corsMiddleware)
+
+	// setup context update  middleware
+	dbService := db_service.NewMongoService[ambulance_wl.Ambulance](db_service.MongoServiceConfig{})
+	defer dbService.Disconnect(context.Background())
+	engine.Use(func(ctx *gin.Context) {
+		ctx.Set("db_service", dbService)
+		ctx.Next()
+	})
+
 	// request routings
 	handleFunctions := &ambulance_wl.ApiHandleFunctions{
 		AmbulanceConditionsAPI:  ambulance_wl.NewAmbulanceConditionsApi(),
 		AmbulanceWaitingListAPI: ambulance_wl.NewAmbulanceWaitingListApi(),
+		AmbulancesAPI:           ambulance_wl.NewAmbulancesApi(),
 	}
 	ambulance_wl.NewRouterWithGinEngine(engine, *handleFunctions)
 	engine.GET("/openapi", api.HandleOpenApi)
--- internal/ambulance_wl/README.md
+++ internal/ambulance_wl/README.md
@@ -13,7 +13,7 @@ To see how to make this your own, look here:
 [README](https://openapi-generator.tech)
 
 - API version: 1.0.0
-- Build date: 2025-03-23T15:42:35.875526521Z[Etc/UTC]
+- Build date: 2025-03-23T16:03:08.916789398Z[Etc/UTC]
 - Generator version: 7.13.0-SNAPSHOT
 
 ### Running the server
